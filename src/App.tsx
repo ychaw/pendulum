0c6e7fede6259b140c6dd1125578a35d1a160ce8
hphaw <44225863+hphaw@users.noreply.github.com>
2021-03-27 18:47:41 +0100

    Added Figma Design Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Pendulum</h1>
-      </header>
+
+      <div className="AnimationWindow">
+
+        <div className="FocusWindow">
+
+          <div className="FocusCard"></div>
+          
+        </div>
+
+        <div className="DetailWindow">
+
+          <div className="DetailTopCard"></div>
+          <div className="DetailCenterCard"></div>
+          <div className="DetailBottomCard"></div>
+
+        </div>
+
+      </div>
+
+      <div className="SettingsWindow">
+
+        <div className="SettingsOscillatorCard"></div>
+        <div className="SettingsEnvelopeCard"></div>
+        <div className="SettingsFilterCard"></div>
+        <div className="SettingsVolumeCard"></div>
+
+      </div>
+
     </div>
   );
 }
