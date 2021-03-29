d368bc423f46b22b5b4685eb52f2472d340a08d3
hphaw <44225863+hphaw@users.noreply.github.com>
2021-03-29 14:58:01 +0200

    changed to grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-      <div className="AnimationWindow">
-
-        <div className="FocusWindow">
-
-          <div className="FocusCard"></div>
-          
-        </div>
-
-        <div className="DetailWindow">
-
-          <div className="DetailTopCard"></div>
-          <div className="DetailCenterCard"></div>
-          <div className="DetailBottomCard"></div>
-
-        </div>
-
-      </div>
-
-      <div className="SettingsWindow">
-
-        <div className="SettingsOscillatorCard"></div>
-        <div className="SettingsEnvelopeCard"></div>
-        <div className="SettingsFilterCard"></div>
-        <div className="SettingsVolumeCard"></div>
-
-      </div>
-
+      <div className="HeaderCard"></div>
+      <div className="FocusCard"></div>
+      <div className="DetailTopCard"></div>
+      <div className="DetailCenterCard"></div>
+      <div className="DetailBottomCard"></div>
+      <div className="SettingsOscillatorCard"></div>
+      <div className="SettingsEnvelopeCard"></div>
+      <div className="SettingsFilterCard"></div>
+      <div className="SettingsVolumeCard"></div>
     </div>
   );
 }
