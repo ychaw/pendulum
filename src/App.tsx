875b025535f0ae068793b1987b747ed6eb3a0bf1
hphaw <44225863+hphaw@users.noreply.github.com>
2021-04-02 18:41:01 +0200

    Added App- and Settings-Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="HeaderCard"></div>
+      <div className="HeaderCard">
+        <h1 className="HeaderText">Pendulum</h1>
+        <h3 className="HeaderText">Oscillator based on a double pendulum by Yannick Clausen & Henry Peters</h3>
+      </div>
       <div className="FocusCard"></div>
       <div className="DetailTopCard"></div>
       <div className="DetailCenterCard"></div>
       <div className="DetailBottomCard"></div>
-      <div className="SettingsOscillatorCard"></div>
-      <div className="SettingsEnvelopeCard"></div>
-      <div className="SettingsFilterCard"></div>
-      <div className="SettingsVolumeCard"></div>
+      <div className="SettingsOscillatorCard">
+        <div className="SettingsHeader">Oscillator</div>
+        <div className="SettingsContent"></div>
+      </div>
+      <div className="SettingsEnvelopeCard">
+        <div className="SettingsHeader">Envelope</div>
+        <div className="SettingsContent"></div>
+      </div>
+      <div className="SettingsFilterCard">
+        <div className="SettingsHeader">Filter</div>
+        <div className="SettingsContent"></div>
+      </div>
+      <div className="SettingsVolumeCard">
+        <div className="SettingsHeader">Volume</div>
+        <div className="SettingsContent"></div>
+      </div>
     </div>
   );
 }
